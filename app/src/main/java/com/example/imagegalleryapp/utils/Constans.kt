9e2858b31780fc4a79c872eaf7c93fc765c1a8e5
labinot <noti-002@hotmail.com>
2024-08-13 19:34:34 +0200

    DownloadBottomSheet

--- app/src/main/java/com/example/imagegalleryapp/utils/Constans.kt
+++ app/src/main/java/com/example/imagegalleryapp/utils/Constans.kt
@@ -1,10 +1,9 @@
 package com.example.imagegalleryapp.utils
 
-import com.example.imagegalleryapp.BuildConfig
 
 object Constans {
 
-    const val API_KEY = BuildConfig.UNSPLASH_API_KEY
+    const val API_KEY = "rGz_gQ2VqwOUSt3KiDIjWiRDDy8_maBkHH1omZ0Rzpg"
    // const val MY_KEY = "rGz_gQ2VqwOUSt3KiDIjWiRDDy8_maBkHH1omZ0Rzpg"
     const val BASE_URL = "https://api.unsplash.com/"
 }
